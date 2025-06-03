cc02bb2fa5624495b08a4fa80af36b558158c5ee
SteveRodkiss <SteveRodkiss@users.noreply.github.com>
2025-06-03 14:35:12 +1200

    added zombie ragdoll

---
+++ zombie/scripts/zombie.gd
@@ -0,0 +1,18 @@
+extends Area3D
+
+@onready var ragdoll : PhysicalBoneSimulator3D = $ZombieIdle/Skeleton3D/PhysicalBoneSimulator3D
+
+# test function
+#func _process(delta: float) -> void:
+	#if Input.is_action_just_pressed("ui_accept"):
+		#print("Key Pressed")
+		#activate_ragdoll()
+
+func activate_ragdoll():
+	if not ragdoll.is_simulating_physics():
+		ragdoll.physical_bones_start_simulation()
+	
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		activate_ragdoll()
